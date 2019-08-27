96c3d60f880235ea459eb8ee37236d33d71bf514
Gilles Coenraets <gilles.coenraets@ingenico.com>
2019-08-27 18:22:21 +0200

    attempt

--- ooja.python.learning/main.py
+++ ooja.python.learning/main.py
@@ -2,15 +2,26 @@ from ooja import network
 from numpy import random
 from numpy import array
 
-f = lambda x: 0 if x < 0 else 1
+shape = array([2, 3, 2, 1])
+n = network.Network(shape, network.Sigmoid())
+x = array([0, 1])
+o = n.forward(x)
 
-n = network.Node(1, f, 0.1)
-l = network.Link()
-l.x = array([0, 1])
-l.w = random.rand(2)
-n.input = l
-s = n.output()
+for l in n.layers:
+    l.display()
+print("o={0}".format(o))
 
-print(l.x)
-print(l.w)
-print(s)
+n.backprop(1)
+for l in n.layers:
+    l.display()
+
+#n = network.Node(1, f, 0.1)
+#l = network.Link()
+#l.x = array([0, 1])
+#l.w = random.rand(2)
+#n.input = l
+#s = n.output()
+
+#print(l.x)
+#print(l.w)
+#print(s)
--- ooja.python.learning/ooja/network.py
+++ ooja.python.learning/ooja/network.py
@@ -1,23 +1,52 @@
 import numpy as np
 
 class Network:
-    def __init__(self, shape, ninput, transfer, threshold):
+    def __init__(self, shape, transfer):
         self.nlayers = shape.size
-
-class Layer:
-    def __init__(self, nneurons, ninput):
-        self.w = np.random.rand(ninput, nneurons)
-        self.b = np.ones(nneurons) * 0.1
+        self.layers = []
+        self.o = []
+        for i in range(1, self.nlayers):
+            self.layers.append(Layer(shape[i], shape[i - 1], transfer))
 
     def forward(self, x):
-        return np.dot(x, self.w)
+        for l in self.layers:
+            x = l.forward(x)
+        self.o = x
+        return self.o
 
-    def backward(self, x, d):
+    def backprop(self, t):
+        self.layers[self.nlayers - 2].e = t - self.o
+        for l in range(self.nlayers - 2, 0, -1):
+            self.layers[l - 1].error(self.layers[l])
+        for l in self.layers:
+            l.w += l.e * l.dx * l.x
 
+class Layer:
+    def __init__(self, nneurons, ninput, transfer):
+        self.w = np.random.rand(ninput, nneurons)
+        self.b = np.ones(nneurons) * 0.1
+        self.transfer = transfer
+        self.e = []
+        self.x = []
+        self.dx = []
+
+    def forward(self, x):
+        self.x = x
+        a = np.dot(self.x, self.w) + self.b
+        self.dx = self.transfer.der(a)
+        return self.transfer.out(a)
+
+    def error(self, layer):
+        self.e = np.dot(layer.w, layer.e)
+
+    def display(self):
+        print("x={0}".format(self.x))
+        print("w={0}".format(self.w))
+        print("b={0}".format(self.b))
 
 class Node:
     """a node in the network"""
-    def __init__(self, id, transfer, bias, is_output = False):
+    def __init__(self, id, transfer, bias, is_output=False):
         self.id = id
         self.transfer = transfer
         self.bias = bias
@@ -37,3 +66,19 @@ class Link:
         self.destination = []
         self.x = []
         self.w = []
+
+class Step:
+    def out(self, n):
+        return np.heaviside(n, 0.5)
+
+    def der(self, n):
+        return np.ones_like(n)
+
+class Sigmoid:
+    def out(self, n):
+        s = 1 / (1 + np.exp(-n)) 
+        return s
+
+    def der(self, n):
+        s = self.out(n)
+        return s * (1 - s)
