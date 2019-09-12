8f173d1e0595f597eef94e7408ef0f8d47ddda32
Gilles Coenraets <gilles.coenraets@ingenico.com>
2019-09-12 20:26:10 +0200

    New design

--- ooja.python.learning/main.py
+++ ooja.python.learning/main.py
@@ -3,46 +3,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 import netdraw as nd
 from sklearn import datasets
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn import metrics
 
-data = np.array([[0, 1],
-     [1, 1],
-     [1, 0],
-     [0, 0]])
-target = np.array([1,
-     0,
-     1,
-     0])
-mapper = [1]
-#s = datasets.load_iris()
-#data = s.data
-#target = s.target
+#data = np.array([[0, 1],
+#     [1, 1],
+#     [1, 0],
+#     [0, 0]])
+#target = np.array([1,
+#     0,
+#     1,
+#     0])
+#mapper = [1]
+s = datasets.load_iris()
+data = s.data
+target = s.target
+mapper = np.unique(target)
 #mapper = np.arange(3)
 ni = data.shape[1]
 no = len(mapper)
 nsamples = len(data)
 
-shape = np.array([ni, 5, no])
-n = network.Network(shape, network.Sigmoid(), lambda o: (o >= 0.5) * 1)
+scaler = MinMaxScaler()
+normalized = scaler.fit_transform(data, target)
 
+onehot = OneHotEncoder(sparse=False, categories='auto')
+tencoded = onehot.fit_transform(target.reshape(len(target), 1))
+
+n = network.Network(lambda o: np.round(o, 2))
+n.addlayer(7, ninput=4, transfer=network.Sigmoid())
+n.addlayer(3, transfer=network.Sigmoid())
 
 e = 1.
 i = 0
 for i in range(1, 5000):
     print('\r'+str(i)+'      '+str(e), sep='', end='', flush=True)
-    if e < 0.06:
+    if e < 0.01:
        break
     e = 0.
-    for x in range(0, len(data)):
-        o = n.forward(data[x])
-        t = (target[x] == mapper) * 1
-        n.backprop(t, e)
-        e += np.mean(np.square(n.e))
-    e = np.sqrt(e/nsamples)
+    #for x in range(0, len(normalized)):
+    y = n.forward(normalized)
+        
+    #t = (target == mapper) * 1
+    n.backprop(tencoded, .1)
+    e = n.E / nsamples
+    #e = 0.5 * np.sqrt(e/nsamples)
 
-for x in range(0, len(data)):
+for x in range(0, len(normalized)):
     print()
-    o = n.forward(data[x])
+    o = n.forward(normalized[x])
     t = (target[x] == mapper) * 1
-    print("x={0} --> t={1} o={2}".format(data[x], t, o))
+    print("x={0} --> t={1} o={2}".format(normalized[x], t, o))
 
 n.display(e, i)
--- ooja.python.learning/ooja/network.py
+++ ooja.python.learning/ooja/network.py
@@ -2,42 +2,45 @@ import numpy as np
 from matplotlib import pylab
 import pylab as plt
 import netdraw as nd
+from sklearn import metrics
 
 class Network:
-    def __init__(self, shape, transfer, threshold):
-        self.shape = shape
-        self.nlayers = shape.size - 1
+    def __init__(self, threshold):
+        self.nlayers = 0
         self.layers = []
-        self.o = 0.
-        self.e = 0.
+        self.y = 0.
+        self.E = 0.
         self.threshold = threshold
-        self.transfer = transfer
-        for i in range(1, self.nlayers + 1):
-            self.layers.append(Layer(shape[i - 1], shape[i], transfer))
 
-    def addlayer(self, ninput, nneurons, transfer):
+    def addlayer(self, nneurons, ninput=None, transfer=None):
+        if ninput == None:
+            ninput = self.layers[-1].nneurons
         self.layers.append(Layer(ninput, nneurons, transfer))
         self.nlayers += 1
 
     def forward(self, x):
         for l in self.layers:
             x = l.forward(x)
-        self.o = x
-        return self.threshold(self.o)
+        self.y = x
+        return self.threshold(self.y)
 
-    def backprop(self, t, e):
-        lr = self.transfer.out(np.abs(e))
-        alpha = 1. -lr
-        self.e = t - self.o
-        self.layers[-1].e = self.e
-        self.layers[-1].dx = self.e * self.layers[-1].transfer.der(self.o)
+    def backprop(self, t, lr):
+        momentum = .9
+        outlayer = self.layers[-1]
+
+        self.E = np.sum(np.square(t - self.y)) / 2
+
+        outlayer.dEdy = t - self.y
+        outlayer.dEdx = outlayer.transfer.der(outlayer.ti) * outlayer.dEdy
+        outlayer.dEdw = np.dot(outlayer.x.T,  outlayer.dEdx)
+        outlayer.dEdb = np.sum(outlayer.dEdx, axis=0)
+
         for l in range(self.nlayers - 1, 0, -1):
-            self.layers[l - 1].loss(self.layers[l])
+            self.layers[l - 1].gradient(self.layers[l])
+
         for l in self.layers:
-            l.dw = (lr * np.atleast_2d(l.x).T * l.dx) + (alpha * l.dw)
-            l.w += l.dw
-            l.db = (lr * l.dx) + (alpha * l.db)
-            l.b += l.db
+            l.w += (lr * l.dEdw)
+            l.b += (lr * l.dEdb)
 
     def display(self, e, i):
         fig = plt.figure(figsize=(12, 12))
@@ -45,10 +48,10 @@ class Network:
         ax.axis('off')
         w = []
         b = []
-        for l in range(0, self.shape.size-1):
+        for l in range(0, self.nlayers):
             w.append(self.layers[l].w)
             b.append(self.layers[l].b)
-        nd.draw_neural_net(ax, .1, .9, .1, .9, self.shape, w, b, i, e)
+        #nd.draw_neural_net(ax, .1, .9, .1, .9, self.shape, w, b, i, e)
         for l in self.layers:
             l.display()
         #plt.show()
@@ -61,19 +64,24 @@ class Layer:
         self.transfer = transfer
         self.e = None
         self.x = None
-        self.o = None
-        self.dx = None
-        self.dw = 0.
-        self.db = 0.
+        self.y = None
+        self.ti = None
+        self.dEdx = None
+        self.dEdy = None
+        self.dEdw = None
+        self.dEdb = None
 
     def forward(self, x):
-        self.x = x
-        self.o = self.transfer.out(np.dot(self.x, self.w) + self.b)
-        return self.o
+        self.x = np.atleast_2d(x)
+        self.ti = np.dot(self.x, self.w) + self.b
+        self.y = self.transfer.out(self.ti)
+        return self.y
 
-    def loss(self, layer):
-        self.e = np.dot(layer.dx, layer.w.T)
-        self.dx = self.e * self.transfer.der(self.o)
+    def gradient(self, layer):
+        self.dEdy = np.dot(layer.dEdx, layer.w.T)
+        self.dEdx = self.transfer.der(self.ti) * self.dEdy
+        self.dEdw = np.dot(self.x.T,  self.dEdx)
+        self.dEdb = np.sum(self.dEdx, axis=0)
 
     def display(self):
         print("x={0}".format(self.x))
@@ -97,3 +105,42 @@ class Sigmoid:
     def der(self, n):
         s = self.out(n)
         return s * (1 - s)
+
+class Relu:
+    def out(self, n):
+        n = np.clip(n, -20, 20)
+        return np.maximum(0.01 * n, n)
+    
+    def der(self, n):
+        gradients = 1. * (n > 0.01)
+        gradients[gradients == 0] = 0.01
+        return gradients
+
+class Softmax:
+    def out(self, n):
+        exps = np.exp(n - n.max())
+        return exps / np.sum(exps)
+
+    def der(self, n):
+        jacobian = np.diag(n)
+        for i in range(len(jacobian)):
+            for j in range(len(jacobian)):
+                if i == j:
+                    jacobian[i][j] = n[i] * (1 - n[j])
+                else:
+                    jacobian[i][j] = -n[i] * n[j]
+        return jacobian
+
+class Loss:
+    def CrossEntropy(self, o, t):
+        return metrics.log_loss(t, o)
+
+    def MeanSquareError(self, o, t):
+        return np.square(t - o).mean()
+
+class Error:
+    def out(self, y, t):
+        return ((y - t) ** 2) / 2
+
+    def der(self, y, t):
+        return y - t
