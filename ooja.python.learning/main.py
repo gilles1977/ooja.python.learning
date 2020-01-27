1fe6a74094d5d69a7ee86b9f0ea220754a67b459
Gilles Coenraets <gilles.coenraets@ingenico.com>
2020-01-27 15:23:32 +0100

    update

--- ooja.python.learning/main.py
+++ ooja.python.learning/main.py
@@ -5,6 +5,7 @@ import netdraw as nd
 from sklearn import datasets
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from sklearn import metrics
+from sklearn.model_selection import train_test_split
 
 #data = np.array([[0, 1],
 #     [1, 1],
@@ -24,6 +25,8 @@ data = scaler.fit_transform(data, target)
 onehot = OneHotEncoder(sparse=False, categories='auto')
 target = onehot.fit_transform(target.reshape(len(target), 1))
 
+X_train, X_test, y_train, y_test = train_test_split(data, target, random_state=42)
+
 #mapper = np.unique(target)
 #mapper = np.arange(3)
 #ni = data.shape[1]
@@ -41,7 +44,7 @@ for i in range(1, 5000):
     print('\r'+str(i)+'      '+str(e), sep='', end='', flush=True)
     if e < 0.1:
       break
-    e = n.sgd(data, target)
+    e = n.sgd(X_train, y_train)
     #e = n.minibatch(data, target, minibatchsize)
     #e = n.batch(data, target)
 
@@ -51,10 +54,9 @@ for i in range(1, 5000):
 
     #e = 0.5 * np.sqrt(e/nsamples)
 
-for x in range(0, len(data)):
+for x in range(0, len(X_test)):
     print()
-    o = n.forward(data[x])
-    t = target[x]
-    print("x={0} --> t={1} o={2}".format(data[x], t, o))
+    o = n.forward(X_test[x])
+    print("x={0} --> t={1} o={2}".format(X_test[x], y_test[x], o))
 
 n.display(e, i)
